10619e1b20a24d9e6a103020adc49d91c5af42fd
yunlinIT <yunlinit@gmail.com>
2024-01-02 17:45:20 +0900

    회원가입 기능 구현, 비밀번호 확인 추가

--- src/Main.java
+++ src/Main.java
@@ -43,8 +43,21 @@ public class Main {
 					}
 					break;
 				}
-				System.out.print("로그인 비밀번호 : ");
-				String loginPw = sc.nextLine();
+				String loginPw = null;
+
+				while (true) {
+					System.out.print("로그인 비밀번호 : ");
+					loginPw = sc.nextLine();
+					System.out.print("로그인 비밀번호 확인: ");
+					String loginPwConfirm = sc.nextLine();
+
+					if (loginPw.equals(loginPwConfirm) == false) {
+						System.out.println("비밀번호 다시 확인해");
+						continue;
+					}
+					break;
+				}
+
 				System.out.print("이름 : ");
 				String name = sc.nextLine();
 
